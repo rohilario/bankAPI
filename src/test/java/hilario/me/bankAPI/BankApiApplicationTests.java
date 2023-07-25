37d4a884467fa319827c936ac5a1e01e3433d945
rohilario <rodrigohilariocomercial@gmail.com>
2023-07-25 11:34:42 -0300

    projeto base com spring

---
+++ src/test/java/hilario/me/bankAPI/BankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hilario.me.bankAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
