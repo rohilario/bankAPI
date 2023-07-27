56c8c913fbe57c3180b35fbaa050a5662fed5d66
rohilario <rodrigohilariocomercial@gmail.com>
2023-07-27 18:50:21 -0300

    Hotfix: CORS Fixed

--- src/main/java/hilario/me/bankAPI/Application.java
+++ src/main/java/hilario/me/bankAPI/Application.java
@@ -3,6 +3,11 @@ package hilario.me.bankAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "") })
 @SpringBootApplication
 public class Application {
 
